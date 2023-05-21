ce2ca41274dbf6c254a401501dd0066633cb3ecc
Radu Cristescu <radu.cx@gmail.com>
2023-05-21 16:50:50 +0100

    Updated example

--- example/example.ps1
+++ example/example.ps1
@@ -1,15 +1,18 @@
 Import-Module 'powershell-jwt'
 
+Function Get-CurrentTimestamp {
+    [int](Get-Date -UFormat %s) # Grab Unix Epoch Timestamp
+}
+
 Function Get-ExpiryTimeStamp {
     param (
         [int]$ValidForSeconds = 30
     )
-    $exp = [int](Get-Date -UFormat %s) + $ValidForSeconds # Grab Unix Epoch Timestamp and add desired expiration.
+    $exp = (Get-CurrentTimestamp) + $ValidForSeconds # Grab Unix Epoch Timestamp and add desired expiration.
     $exp
 }
 
 $Issuer = 'example'
-$hmacSecret = [Convert]::FromBase64String('kFmnkHSH2E7IZ2HDV9QDDo2jv3tVfqWmRPnqT+y7vfN5O0ZbXc4cnxxhs1TfP0+hFKKAbc2MFwRN75o72M33Pcd2Wt8iBBQvNHXwp1XOUZ0zRTwQ1IO+gJGaVRv0OvSz5aDo+41rq0scZnpUSmR30r9curUUlFKVIHF8mE9NMQJx7ojp1WlJBss8OvIwG2pw/v74S33MkkElZibjEuydrR3k0vPBI7JgQpJjAPeLzzf+gfBZZOgKOS2U9ajLcE5TtAL4Wr8U/AEYDFSct362LlgTx5zMT+BaXQNIcJxxyBnOEIUhcxfykQ1tJrr0T8dG/wrvfkGuaqEzd8eEOkK+bA==')
 $hmacSecret = [Convert]::FromBase64String('kFmnkHSH2E7IZ2HDV9QDDo2jv3tVfqWmRPnqT+y7vfN5O0ZbXc4cnxxhs1TfP0+hFKKAbc2MFwRN75o72M33Pcd2Wt8iBBQvNHXwp1XOUZ0zRTwQ1IO+gJGaVRv0OvSz5aDo+41rq0scZnpUSmR30r9curUUlFKVIHF8mE9NMQJx7ojp1WlJBss8OvIwG2pw/v74S33MkkElZibjEuydrR3k0vPBI7JgQpJjAPeLzzf+gfBZZOgKOS2U9ajLcE5TtAL4Wr8U/AEYDFSct362LlgTx5zMT+BaXQNIcJxxyBnOEIUhcxfykQ1tJrr0T8dG/wrvfkGuaqEzd8eEOkK+bA==')
 $rsaPrivateKey = Get-Content "./private_key.pem" -AsByteStream
 $rsaPublicKey = Get-Content "./public_key.pem" -AsByteStream
@@ -32,8 +35,8 @@ $jwt2 = New-JWT -Algorithm 'RS256' -Issuer $Issuer -SecretKey $rsaPrivateKey -Ex
 Write-Host "HS256 signed token:" $jwt1
 Write-Host "RS256 signed token:" $jwt2
 
-$valid1 = Confirm-JWT -JWT $jwt1 -Key $hmacSecret
-$valid2 = Confirm-JWT -JWT $jwt2 -Key $rsaPublicKey
+$valid1 = Confirm-JWT -JWT $jwt1 -AcceptedAlgorithm 'HS256' -Key $hmacSecret
+$valid2 = Confirm-JWT -JWT $jwt2 -AcceptedAlgorithm 'RS256' -Key $rsaPublicKey
 
 Write-Host "HS256 signature is valid:" $valid1.isSignatureValid
 Write-Host "HS256 confirmation:" ($valid1 | ConvertTo-Json)
